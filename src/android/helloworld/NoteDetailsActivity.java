42139d2d35a87fb8835b72d6634e5a116cc750c3
Felipe Silveira <webfelipe@gmail.com>
2014-02-11 16:50:30 -0200

    layout performance pitfals

---
+++ src/android/helloworld/NoteDetailsActivity.java
@@ -0,0 +1,32 @@
+package android.helloworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class NoteDetailsActivity extends BaseActivity {
+
+	protected static final String NOTE = "note";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.details);
+		
+		TextView noteTextView = (TextView) findViewById(R.id.noteTextView);
+		noteTextView.setText(getIntent().getExtras().getString(NOTE, ""));
+		
+		Button exitButton = (Button) findViewById(R.id.button1);
+		exitButton.setOnClickListener(new OnClickListener() {
+			  public void onClick(View v) {
+	        	  finish();
+	          }
+		});
+	}
+
+}
---
+++ src/android/helloworld/adapter/NotesCursorAdapter.java
@@ -0,0 +1,34 @@
+package android.helloworld.adapter;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.helloworld.QuickNotesProvider;
+import android.helloworld.R;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+ 
+public class NotesCursorAdapter extends CursorAdapter {
+ 
+    public NotesCursorAdapter(Context context, Cursor c) {
+        super(context, c);
+    }
+ 
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View retView = inflater.inflate(R.layout.list_item, parent, false);
+ 
+        return retView;
+    }
+ 
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+        TextView textViewNote = (TextView) view.findViewById(R.id.text);
+        textViewNote.setText(cursor.getString(cursor.getColumnIndex(QuickNotesProvider.Notes.TEXT)));
+ 
+    }
+}
